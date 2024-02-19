ce42bc9267504bedd4827d72fb3aad63124c2aa4
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-02-20 04:33:55 +0530

    Create P8.cpp

---
+++ P8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    if(n%2 == 0)
+    {
+        cout<<"It is an Even number";
+    }
+    else
+    {
+        cout<<"It is an Odd number";
+    }
+    return 0;
+}
