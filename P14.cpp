070c1c5e0e20cc4df01147f3044588df649ed25e
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-02-20 15:37:49 +0530

    Create P14.cpp
    Enter no. of rows : 5
    A
    A B
    A B C
    A B C D
    A B C D E

---
+++ P14.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter no. of rows : ";
+    cin>>n;
+    
+    for(int row=1;row<=n;row++)
+    {
+        for(int col=1;col<=n;col++)
+        {
+            if(row>=col)
+            {
+                cout<<char(col+64)<<" ";
+            }
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
