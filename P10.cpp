f3661620b145bdaa62ad4a29053a07fc21761927
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-02-20 14:07:13 +0530

    Create P10.cpp

---
+++ P10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum = 0;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        sum+=i;
+    }
+    cout<<"The sum is "<<sum;
+
+    return 0;
+}
