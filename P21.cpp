ea29080cd1306f5edbf8e87a4300b05333fa7251
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-02-24 18:16:50 +0530

    Create P21.cpp

---
+++ P21.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    int fact = 1;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        fact = fact*i;
+    }
+    cout<<"The factorial of "<<n<<" is "<<fact;
+    return 0;
+}
