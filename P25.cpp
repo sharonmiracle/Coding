228efd2800f90dce5c93bfcb10d0dc56ba6f18f0
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-03-05 14:54:09 +0530

    Create P25.cpp

---
+++ P25.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    int m=n;
+    m = m*(m+1)/2;
+    cout<<"The sum is "<<m;
+
+    return 0;
+}
